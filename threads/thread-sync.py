102ba930c8a0e08db5a5ba05a6ecfa727769c7f8
Andre Ribeiro <andre.ribeiro.srs@gmail.com>
2025-02-16 17:54:05 -0300

    Code improvements.

--- threads/thread-sync.py
+++ threads/thread-sync.py
@@ -17,8 +17,8 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Constants
-THREADS_QTY: int = 99
-STATE_CHANGE_DELAY: float = 0.5
+THREADS_QTY: int = 999
+STATE_CHANGE_DELAY: float = 1.0
 
 # Value definitions
 ON = True
@@ -28,12 +28,12 @@ OFF = False
 class AppControlFlags:
     """
     This class provides flags for controlling application behavior, such as whether to keep running,
-    wait between operations, and tracking operational counters. It facilitates monitoring system usage through a counter mechanism.
+    wait between operations, and track operational counters. It facilitates monitoring system usage through a counter mechanism.
 
     Attributes:
-        keep_running (bool): Flag indicating if the application should continue executing.
-        wait (bool): Flag determining operation timing intervals.
-        __WORK_COUNTER (Dict[str, int]): Dictionary tracking counts of various operations.
+        _keep_running (bool): Flag indicating if the application should continue executing.
+        _wait (bool): Flag determining operation timing intervals.
+        _WORK_COUNTER (Dict[str, int]): Dictionary tracking counts of various operations.
 
     Methods:
         add_counter(name: str) -> None:
@@ -52,12 +52,17 @@ class AppControlFlags:
 
         get_all_values() -> Dict[str, int]:
             Returns a dictionary containing all tracked operation counters for program-wide analysis or reporting.
+
+        values_balanced() -> bool:
+            Checks if all tracked operation counts in the __WORK_COUNTER dictionary are equal and greater than zero.
+            Returns:
+                bool: True if all values are the same and greater than zero, False otherwise.
     """
 
     def __init__(self) -> None:
-        self.keep_running = True
-        self.wait = True
-        self.__WORK_COUNTER: Dict[str, int] = {}
+        self._keep_running = True
+        self._wait = True
+        self._WORK_COUNTER: Dict[str, int] = {}
 
     @property
     def keep_running(self) -> bool:
@@ -86,7 +91,7 @@ class AppControlFlags:
         Args:
             name (str): Name of the operation to track.
         """
-        self.__WORK_COUNTER[name] = 0
+        self._WORK_COUNTER[name] = 0
 
     def get_counter(self, name: str) -> int:
         """
@@ -98,7 +103,7 @@ class AppControlFlags:
         Returns:
             int: Current counter value for the operation.
         """
-        return self.__WORK_COUNTER.get(name, 0)
+        return self._WORK_COUNTER.get(name, 0)
 
     def increment_counter(self, name: str) -> None:
         """
@@ -107,8 +112,8 @@ class AppControlFlags:
         Args:
             name (str): Name of the operation to update.
         """
-        if name in self.__WORK_COUNTER:
-            self.__WORK_COUNTER[name] += 1
+        if name in self._WORK_COUNTER:
+            self._WORK_COUNTER[name] += 1
 
     def min_counter(self) -> str:
         """
@@ -118,7 +123,17 @@ class AppControlFlags:
         Returns:
             str: Name of the operation with the minimal count.
         """
-        return min(self.__WORK_COUNTER.keys(), key=lambda k: self.__WORK_COUNTER[k])
+        return min(self._WORK_COUNTER.keys(), key=lambda k: self._WORK_COUNTER[k])
+
+    def values_balanced(self) -> bool:
+        """
+        Check if all values in the __WORK_COUNTER dictionary are equal and greater than zero.
+
+        Returns:
+            bool: True if all values are the same and greater than zero, False otherwise.
+        """
+        values = set(self._WORK_COUNTER.values())
+        return len(values) == 1 and next(iter(values)) > 0
 
     def get_all_values(self) -> Dict[str, int]:
         """
@@ -127,7 +142,7 @@ class AppControlFlags:
         Returns:
             dict: Mapping from operation names to their respective counter values.
         """
-        return self.__WORK_COUNTER.copy()
+        return self._WORK_COUNTER.copy()
 
 
 class Job(threading.Thread):
@@ -139,7 +154,6 @@ class Job(threading.Thread):
 
     Attributes:
         control (AppControlFlags): Shared application control flags for system state.
-        lock (threading.Lock): Coordination lock for thread-safe operations.
         condition (threading.Condition): Synchronization primitive for wait/notify.
         active (bool): Indication about what is the Thread state.Defaults to OFF.
         thread_name (str): Unique identifier for this job instance.
@@ -148,7 +162,6 @@ class Job(threading.Thread):
     def __init__(
         self,
         control: AppControlFlags,
-        lock: threading.Lock,
         condition: threading.Condition,
         thread_name: str,
     ) -> None:
@@ -157,7 +170,6 @@ class Job(threading.Thread):
 
         Args:
             control: Shared application state controller
-            lock: Mutual exclusion lock for resource protection
             condition: Coordination primitive for thread scheduling
             active: Hold the thread state (ON or OFF)
             thread_name: Unique identifier for this job
@@ -166,9 +178,9 @@ class Job(threading.Thread):
         self.name = thread_name
 
         self._control: AppControlFlags = control
-        self._lock: threading.Lock = lock
         self._condition: threading.Condition = condition
         self._active: bool = OFF  # Initial activity state
+
         self._control.add_counter(name=self.name)
         self.start()
 
@@ -179,7 +191,7 @@ class Job(threading.Thread):
         Returns:
             bool: True if the application should continue running, False otherwise.
         """
-        with self._lock:
+        with self._condition:
             return self._control.keep_running
 
     def __waiting(self) -> None:
@@ -198,11 +210,9 @@ class Job(threading.Thread):
                     break
 
                 if not got_priority:
-                    logger.debug(f"[{self.name}] Waiting for priority...")
+                    logger.debug(f"[{self.name}] Waiting for priority [Global QTY].")
                 else:
-                    logger.debug(
-                        f"[{self.name}] Waiting for global wait flag to be cleared..."
-                    )
+                    logger.debug(f"[{self.name}] Waiting for priority [Global FLAG].")
 
                 # Wait for a notification
                 self._condition.wait()
@@ -224,6 +234,12 @@ class Job(threading.Thread):
             f"[{self.name}] Toggled {self._control.get_counter(self.name)} times."
         )
 
+    def __jobs_balanced(self) -> bool:
+        """Checks if jobs are balanced using 'self._control.values_balanced()',
+        ensuring thread safety with 'self._condition'."""
+        with self._condition:
+            return self._control.values_balanced()
+
     def run(self) -> None:
         """
         Main execution loop managing task lifecycle and coordination.
@@ -240,7 +256,7 @@ class Job(threading.Thread):
 
                 # Core task execution block
                 self._active = not self._active
-                logger.debug(f"{self.name}] Task state update: {self._active}")
+                logger.debug(f"[{self.name}] Task state update: {self._active}")
 
                 # Stop the thread for a while before changing its state
                 # time.sleep(STATE_CHANGE_DELAY)
@@ -251,26 +267,29 @@ class Job(threading.Thread):
                 # Stop the thread for a while after changed its the State
                 time.sleep(STATE_CHANGE_DELAY)
 
-            logger.info(f"Job '{self.name}' completed successfully")
+            logger.info(f"[{self.name}] Finished successfully!")
 
         except Exception as e:
             logger.error(f"Task failure in '{self.name}': {e}", exc_info=False)
 
 
 def handle_sigint(
-    app_control_flags: AppControlFlags, lock_flag: threading.Lock, sig: int, frame
+    app_control_flags: AppControlFlags,
+    condition_flag: threading.Condition,
+    sig: int,
+    frame,
 ) -> None:
     """
     Handle SIGINT (Ctrl+C) signal for graceful shutdown.
 
     Args:
         app_control_flags (AppControlFlags): Control flags for application state.
-        lock_flag (threading.Lock): Synchronization lock.
+        condition_flag (threading.Condition): Coordination primitive for thread scheduling.
         sig (int): Received signal number.
         frame (FrameType): Current stack frame.
     """
     logger.info("Ctrl+C detected, shutting down...")
-    with lock_flag:
+    with condition_flag:
         app_control_flags.keep_running = False
 
 
@@ -286,8 +305,8 @@ if __name__ == "__main__":
         lock_flag: threading.Lock = threading.Lock()
         condition_flag: threading.Condition = threading.Condition(lock=lock_flag)
 
-        # Create a partial function to handle SIGINT with control_flags and lock_flag
-        sigint_handler = partial(handle_sigint, app_control_flags, lock_flag)
+        # Create a partial function to handle SIGINT with control_flags and condition_flag
+        sigint_handler = partial(handle_sigint, app_control_flags, condition_flag)
         signal.signal(signal.SIGINT, sigint_handler)
 
         # Stores Job thread objects and later joins them.
@@ -297,28 +316,27 @@ if __name__ == "__main__":
         for i in range(THREADS_QTY):
             t = Job(
                 control=app_control_flags,
-                lock=lock_flag,
                 condition=condition_flag,
                 thread_name=str(uuid.uuid4().hex),
             )
-
             threads.append(t)
 
-        # Signalize the threads after they are all ready to start working
-        time.sleep(1)
+        # Make sure all Threads have started
+        while len(threads) < THREADS_QTY:
+            # Do nothing
+            pass
+
+        # Signalize the threads they are ready to start working
         with condition_flag:
             app_control_flags.wait = False
             condition_flag.notify_all()
 
         # Keep the main thread running until the application is stopped
-        while app_control_flags.keep_running:
-            pass
+        for t in threads:
+            # Wait for all threads to finish
+            t.join()
 
         logger.info("Releasing resources ...")
-
-        # Wait for all threads to finish
-        for t in threads:
-            t.join()
 
         # Fetch the results
         data = app_control_flags.get_all_values()
